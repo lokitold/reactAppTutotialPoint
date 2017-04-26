dc78938060a9ae2c912c5e2808c965a184b6a171
victor <vico.16c@gmail.com>
2017-04-26 18:11:11 -0500

    cambio

--- App.jsx
+++ App.jsx
@@ -1,42 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
-            data:
-                [
-                    {
-                        "id":1,
-                        "name":"Foo",
-                        "age":"20"
-                    },
-
-                    {
-                        "id":2,
-                        "name":"Bar",
-                        "age":"30"
-                    },
-
-                    {
-                        "id":3,
-                        "name":"Baz",
-                        "age":"40"
-                    }
-                ]
+            header: "Header from props...",
+            "content": "Content from props..."
         }
     }
 
     render() {
         return (
             <div>
-                <Header/>
-                <table>
-                    <tbody>
-                    {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
-                    </tbody>
-                </table>
+                <Header headerProp = {this.state.header}/>
+                <Content contentProp = {this.state.content}/>
             </div>
         );
     }
@@ -46,20 +24,18 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-                <h1>Header</h1>
+                <h1>{this.props.headerProp}</h1>
             </div>
         );
     }
 }
 
-class TableRow extends React.Component {
+class Content extends React.Component {
     render() {
         return (
-            <tr>
-                <td>{this.props.data.id}</td>
-                <td>{this.props.data.name}</td>
-                <td>{this.props.data.age}</td>
-            </tr>
+            <div>
+                <h2>{this.props.contentProp}</h2>
+            </div>
         );
     }
 }
